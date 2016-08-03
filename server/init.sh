47a8deeac1c8ee5839038ef2ea2aea0e6655c6ef
yids <jenda@localhost>
2016-08-03 09:01:28 +0200

    allow specifying bind address on init.sh command line

--- server/init.sh
+++ server/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 device rate ppm"
+if [ $# -ne 4 ]; then
+  echo "Usage: $0 device rate ppm listen-address"
   exit 2
 fi
 
@@ -9,13 +9,15 @@ sdr="/tmp/sdrpipe"
 tune="/tmp/tunepipe"
 gain="30"
 ppm="$3"
+sample_rate="$2"
+listen_address="$4"
 freq="$(( 100 * 1000 * 1000 ))"
 
 [ ! -p "$sdr" ] && mkfifo "$sdr"
 [ ! -p "$tune" ] && mkfifo "$tune"
 
 # set -b :: for ipv4 and ipv6 all interfaces
-./server -s 2048000 -p "$ppm" -f "$freq" -i "$tune" -o "$sdr" -f "$freq" -g "$gain" -a -s "$2" -b 127.0.0.1 -t 4444 -w 1024 &
+./server -p "$ppm" -f "$freq" -i "$tune" -o "$sdr" -f "$freq" -g "$gain" -a -s "$sample_rate" -b "$listen_address" -t 4444 -w 1024 &
 spid=$!
 
 echo "Server PID $spid, use 'gdb ./server $spid -ex c' to debug"
